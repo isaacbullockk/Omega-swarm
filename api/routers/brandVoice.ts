fe2036d15ff44718a4953615d2a004d5e466e3ee
isaacbullockk <isaac.bullock.k@gmail.com>
2026-07-15 12:07:56 +0200

    api: brandVoice router

---
+++ api/routers/brandVoice.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { getBrandVoice, saveBrandVoice } from "../../db/store";
+
+export const brandVoiceRouter = router({
+  get: publicProcedure.query(async () => {
+    return await getBrandVoice();
+  }),
+
+  save: publicProcedure
+    .input(z.object({
+      tone: z.string(),
+      description: z.string(),
+      samples: z.array(z.string()).optional(),
+    }))
+    .mutation(async ({ input }) => {
+      return await saveBrandVoice(input);
+    }),
+});
