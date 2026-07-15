9ba67dc8e7e08b0cdb826bed6cb16155667e024e
isaacbullockk <isaac.bullock.k@gmail.com>
2026-07-15 12:07:57 +0200

    api: social router

---
+++ api/routers/social.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+import { getSocialAccounts, createSocialAccount, deleteSocialAccount } from "../../db/store";
+
+export const socialRouter = router({
+  list: publicProcedure.query(async () => {
+    return await getSocialAccounts();
+  }),
+
+  create: publicProcedure
+    .input(z.object({
+      platform: z.string(),
+      handle: z.string(),
+      accountName: z.string(),
+      accessToken: z.string().optional(),
+    }))
+    .mutation(async ({ input }) => {
+      return await createSocialAccount(input);
+    }),
+
+  delete: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ input }) => {
+      await deleteSocialAccount(input.id);
+      return { success: true };
+    }),
+
+  disconnect: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ input }) => {
+      await deleteSocialAccount(input.id);
+      return { success: true };
+    }),
+});
